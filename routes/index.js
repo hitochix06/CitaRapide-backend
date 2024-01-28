d3910bdde23a8ceae491443e8f0400dfeb1352f3
hitochix06 <pachara.philibert@hotmail.fr>
2024-01-29 00:06:58 +0100

    Add citation model import to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-
+const citation = require("../models/citations");
 
 // afficher toutes les citations
 router.get('/citations', async (req, res) => {
